5a253422ca89ba0bb68a7830c9d01a5e43b7fd9e
Samuel Johnson <sgjohns1@svsu.edu>
2025-04-12 17:28:48 -0400

    Edited the Asset model fields.

--- assets/models.py
+++ assets/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Asset(models.Model):
-    asset_manufacturer = models.CharField(max_length=100)
-    asset_model = models.CharField(max_length=100)
-    asset_sn = models.CharField(max_length=50)
-    asset_cost = models.CharField(max_length=7)
-    asset_purchaseDate = models.DateField
-    asset_eolDate = models.DateField
-    asset_owner = models.EmailField
-    asset_notes = models.TextField
+    purchase_date_default = datetime.today()
+    eol_date_default = datetime.today()
+    asset_manufacturer = models.CharField(max_length=100, help_text="The manufacturer of the asset e.g. Dell, HP.")
+    asset_model = models.CharField(max_length=100, help_text="The model of the asset e.g. R740.")
+    asset_sn = models.CharField(max_length=50, help_text="Enter the serial number assigned by the manufacturer.")
+    asset_cost = models.CharField(max_length=7,help_text="Enter the cost of the hardware. Do not include any associated services.")
+    asset_purchaseDate = models.DateField(default=purchase_date_default)
+    asset_eolDate = models.DateField(default=eol_date_default)
+    asset_notes = models.TextField(blank=True)
