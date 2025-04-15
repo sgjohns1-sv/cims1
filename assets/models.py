f5b29418c02497aee0ce0681fc295f73178b8269
Samuel Johnson <sgjohns1@svsu.edu>
2025-04-15 06:58:04 -0400

    Added location field to asset.

--- assets/models.py
+++ assets/models.py
@@ -8,6 +8,7 @@ class Asset(models.Model):
     eol_date_default = datetime.today()
     asset_manufacturer = models.CharField(max_length=100, help_text="The manufacturer of the asset e.g. Dell, HP.")
     asset_model = models.CharField(max_length=100, help_text="The model of the asset e.g. R740.")
+    asset_location = models.CharField(max_length=50, help_text="The room the asset is in, e.g. SE175.", default="Science East")
     asset_sn = models.CharField(max_length=50, help_text="Enter the serial number assigned by the manufacturer.")
     asset_cost = models.CharField(max_length=7,help_text="Enter the cost of the hardware. Do not include any associated services.")
     asset_purchaseDate = models.DateField(default=purchase_date_default)
