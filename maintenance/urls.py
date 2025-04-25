a4b685cfba058f53d0367c8a0e36b0f94e204192
Samuel Johnson <sgjohns1@svsu.edu>
2025-04-25 13:39:11 -0400

    Finished maintenance record update changes.

--- maintenance/urls.py
+++ maintenance/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import MaintenanceListView, MaintenanceAddView, MaintenanceDetailView
+from .views import MaintenanceListView, MaintenanceAddView, MaintenanceUpdateView, MaintenanceDetailView
 
 from . import views
 
 urlpatterns=[    
     path("", MaintenanceListView.as_view(), name="maintenance_list"),
-    path("<int:pk>", MaintenanceDetailView.as_view(), name="maintenance_detail"),
+    path("<int:pk>/", MaintenanceDetailView.as_view(), name="maintenance_detail"),
     path("add/", MaintenanceAddView.as_view(), name="maintenance_add"),
+    path("<int:pk>/update/", MaintenanceUpdateView.as_view(), name="maintenance_update")
     ]
