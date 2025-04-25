a5fbadfd13f720529592b0870eeaf0b8cf862209
Samuel Johnson <sgjohns1@svsu.edu>
2025-04-25 13:29:08 -0400

    Wiring up maintenance record upate.

--- maintenance/views.py
+++ maintenance/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Maintenance
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 # Create your views here.
 
@@ -13,6 +13,11 @@ class MaintenanceDetailView(DetailView):
     model = Maintenance
 
 class MaintenanceAddView(CreateView):
-    template_name="maintenance/add.html"
+    template_name="maintenance/addview.html"
     model = Maintenance
+    fields = ["maint_assetID", "maint_title", "maint_description", "maint_cost", "maint_date"]
+
+class MaintenanceUpdateView(UpdateView):
+    template_name="maintenance/updateview.html"
+    model=Maintenance
     fields = ["maint_assetID", "maint_title", "maint_description", "maint_cost", "maint_date"]
