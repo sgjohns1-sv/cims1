982a020fdb14d3ef8d2b170cbd17bc9168a85c7b
Samuel Johnson <sgjohns1@svsu.edu>
2025-04-26 12:59:25 -0400

    Comments and changes to home application.

--- home/views.py
+++ home/views.py
@@ -2,13 +2,13 @@ from django.views.generic import TemplateView
 from assets.models import Asset
 from maintenance.models import Maintenance
 
-# Create your views here.
+#Class based template view
 
 class HomeView(TemplateView):
-    template_name="index.html"
-    def get_context_data(self, **kwargs):
+    template_name="home/index.html" #Sets the name of the template to be used to render the page.
+    def get_context_data(self, **kwargs): #Function to return the context to be used when rendering the home page.
         context = super().get_context_data(**kwargs)
-        context["asset_count"] = Asset.objects.count()
-        context["recent_maintenance"] = Maintenance.objects.filter(date__year=2025)
+        context["asset_count"] = Asset.objects.count() #Gets the current count of assets in the system.
+        context["recent_maintenance"] = Maintenance.objects.filter(date__year=2025) #Gets the most recent maintenance records
         return context
     
