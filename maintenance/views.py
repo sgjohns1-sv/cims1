a9843da351f0cdb69a304e5619fc0c55953fa2d1
Samuel Johnson <sgjohns1@svsu.edu>
2025-04-26 21:39:02 -0400

    Updates to maintenance application.

--- maintenance/views.py
+++ maintenance/views.py
@@ -1,11 +1,18 @@
 from django.urls import reverse_lazy
 from .models import Maintenance
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.db.models import Q
 
 #Class based view to list the maintenance records stored in the system.
 class MaintenanceListView(ListView):
     template_name="maintenance/index.html"
     model = Maintenance
+    def get_queryset(self):
+        if ('search' not in self.request.GET.keys()):
+            maintenance_list = Maintenance.objects.all()
+        else:
+            maintenance_list = Maintenance.objects.filter(Q(title__icontains=self.request.GET['search']) | Q(cost__icontains=self.request.GET['search']) | Q(description__icontains=self.request.GET['search']) | Q(date__icontains=self.request.GET['search']))
+        return maintenance_list
 
 #Class based view used to serve the detailed view of an asset.
 class MaintenanceDetailView(DetailView):
