3535f00a5dec0821e7342995e310aa45ca42a0c5
Samuel Johnson <sgjohns1@svsu.edu>
2025-04-26 22:11:12 -0400

    Fixed URL path.

--- maintenance/urls.py
+++ maintenance/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 #Specifies the URL patterns to use after the initial requst is routed using the master url list for all apps.
 urlpatterns=[    
-    path("", MaintenanceListView.as_view(), name="maintenance_listing"),
+    path("", MaintenanceListView.as_view(), name="maintenance_list"),
     path("<int:pk>/", MaintenanceDetailView.as_view(), name="maintenance_detail"),
     path("add/", MaintenanceAddView.as_view(), name="maintenance_add"),
     path("<int:pk>/update/", MaintenanceUpdateView.as_view(), name="maintenance_update"),
